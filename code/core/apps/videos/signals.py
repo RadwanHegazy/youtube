cf47668ddaaa5b75280469aba0de719357cfeb49
RadwanHegazy <radwangaber11@gmail.com>
2025-04-06 07:15:03 +0200

    final stpes

--- code/core/apps/videos/signals.py
+++ code/core/apps/videos/signals.py
@@ -3,19 +3,18 @@ from .models import Video
 from django.dispatch import receiver
 from django.core.cache import cache
 from .documents import VideoDocument
+from elasticsearch.helpers.errors import BulkIndexError
 
 @receiver(post_save, sender=Video)
 def update_cache(created, instance, **other) : 
     cache.delete('videos')
-
+    VideoDocument().update(instance)
+    
 @receiver(post_delete, sender=Video)
 def update_cache(instance, **other) : 
     cache.delete('videos')
+    try :
+        VideoDocument().update(instance, action='delete')
+    except BulkIndexError:
+        pass
 
-@receiver(post_save, sender=Video)
-def update_video_document(sender, instance, **kwargs):
-    VideoDocument().update(instance)
-
-@receiver(post_delete, sender=Video)
-def delete_video_document(sender, instance, **kwargs):
-    VideoDocument().update(instance, action='delete')
